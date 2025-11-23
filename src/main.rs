25d0acb8907bfe4c6b49561adfa75d2cfd942738
Tomasz Zieliński <zielinski.x360@gmail.com>
2025-11-23 20:05:39 +0100

    qs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+// Tracking Issue for const_cmp of std::any::TypeId:
+// https://github.com/rust-lang/rust/issues/143800
+// Solution with abusing auto-deref specialization:
+// https://stackoverflow.com/a/77548783
+// Solution with procedural macro which injects a Uid field containing a typenum number:
+// https://users.rust-lang.org/t/compile-time-check-if-two-types-are-equal/59521
+// Type level programming examples:
 // https://github.com/insou22/typing-the-technical-interview-rust/blob/main/src/main.rs
 // https://aphyr.com/posts/342-typing-the-technical-interview
-use std::marker::PhantomData;
 use std::ops::BitOr;
-////////// List //////////
-
-struct HNil;
-struct HCons<X, Xs>(PhantomData<(X, Xs)>);
-
-////////// UIDEquals //////////
-
+use frunk::{HCons, HNil};
 use typenum::private::IsEqualPrivate;
 use typenum::{B0, B1, Bit, Cmp, IsEqual, U, Unsigned};
+
+////////// UIDEquals //////////
 
 pub trait ParamValue {
     type UID: Unsigned;
@@ -38,15 +40,15 @@ impl<Needle: ParamValue> Contains<Needle> for HNil {
     type Output = B0;
 }
 
-impl<Needle: ParamValue, ThisHead: ParamValue, Tail: Contains<Needle>> Contains<Needle>
-    for HCons<ThisHead, Tail>
+impl<Needle: ParamValue, Head: ParamValue, Tail: Contains<Needle>> Contains<Needle>
+    for HCons<Head, Tail>
 where
-    Needle::UID: Cmp<ThisHead::UID>,
-    Needle::UID: IsEqualPrivate<ThisHead::UID, <Needle::UID as Cmp<ThisHead::UID>>::Output>,
-    <Needle::UID as IsEqualPrivate<ThisHead::UID, <Needle::UID as Cmp<ThisHead::UID>>::Output>>::Output: BitOr<<Tail as Contains<Needle>>::Output>,
-    <<Needle::UID as IsEqualPrivate<ThisHead::UID, <Needle::UID as Cmp<ThisHead::UID>>::Output>>::Output as BitOr<<Tail as Contains<Needle>>::Output>>::Output: Bit,
+    Needle::UID: Cmp<Head::UID>,
+    Needle::UID: IsEqualPrivate<Head::UID, <Needle::UID as Cmp<Head::UID>>::Output>,
+    <Needle::UID as IsEqualPrivate<Head::UID, <Needle::UID as Cmp<Head::UID>>::Output>>::Output: BitOr<<Tail as Contains<Needle>>::Output>,
+    <<Needle::UID as IsEqualPrivate<Head::UID, <Needle::UID as Cmp<Head::UID>>::Output>>::Output as BitOr<<Tail as Contains<Needle>>::Output>>::Output: Bit,
 {
-    type Output = <<(Needle, ThisHead) as UIDEquals>::Output as BitOr<<Tail as Contains<Needle>>::Output >>::Output;
+    type Output = <<(Needle, Head) as UIDEquals>::Output as BitOr<<Tail as Contains<Needle>>::Output >>::Output;
 }
 
 ////////// PrependIf //////////
@@ -55,11 +57,11 @@ trait PrependIf {
     type Output;
 }
 
-impl<TheHead, Tail> PrependIf for (B1, TheHead, Tail) {
-    type Output = HCons<TheHead, Tail>;
+impl<Head, Tail> PrependIf for (B1, Head, Tail) {
+    type Output = HCons<Head, Tail>;
 }
 
-impl<TheHead, Tail> PrependIf for (B0, TheHead, Tail) {
+impl<Head, Tail> PrependIf for (B0, Head, Tail) {
     type Output = Tail;
 }
 
@@ -73,19 +75,19 @@ impl<Rhs> Intersection<Rhs> for HNil {
     type Output = HNil;
 }
 
-impl<TheHead: ParamValue, Tail, Rhs, TailFilterOutput> Intersection<Rhs> for HCons<TheHead, Tail>
+impl<Head: ParamValue, Tail, Rhs, TailFilterOutput> Intersection<Rhs> for HCons<Head, Tail>
 where
     Tail: Intersection<Rhs, Output = TailFilterOutput>,
-    Rhs: Contains<TheHead>,
+    Rhs: Contains<Head>,
     (
-        <Rhs as Contains<TheHead>>::Output,
-        TheHead,
+        <Rhs as Contains<Head>>::Output,
+        Head,
         TailFilterOutput,
     ): PrependIf,
 {
     type Output = <(
-        <Rhs as Contains<TheHead>>::Output,
-        TheHead,
+        <Rhs as Contains<Head>>::Output,
+        Head,
         <Tail as Intersection<Rhs>>::Output,
     ) as PrependIf>::Output;
 }
@@ -124,6 +126,7 @@ fn main() {
     println!(
         "{}",
         std::any::type_name::<<List1 as Intersection<List2>>::Output>()
-            .replace("nine_queens::", "")
+            .replace("frunk_core::hlist::", "")
+            .replace("hlist_intersection_assuming_manual_id_assignment::", "")
     );
 }
